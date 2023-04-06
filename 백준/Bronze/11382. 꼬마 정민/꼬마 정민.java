f937acdc57aebc409124972c098e83b2be540400
connermaker <39045465+connermaker@users.noreply.github.com>
2023-04-06 20:22:26 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long num1,num2,num3;
+        num1 = sc.nextLong();
+        num2 = sc.nextLong();
+        num3 = sc.nextLong();
+        System.out.println(num1+num2+num3);
+    }
+}
